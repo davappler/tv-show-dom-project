40c0d08c357197d5b77430dc7559237a252535c7
davibaweja <davibaweja@gmail.com>
2020-09-10 23:20:29 +0100

    V2.9 JS3WEEK2
    Completed the level 400 of the project.

--- script.js
+++ script.js
@@ -1,165 +1,102 @@
 
-// things to do
-// Allow search field to search for multiple words too. // verify this with others
+// these variables are made global so that when there values are changed then
+// it can be reflected to the event listeners present in setup function. 
+// Here we are giving them some initial value so that our page does not start empty when it is open for the first time.
 
-// now it is working for multiple words but I want it to search the words in order as we type
-// ask for a better way to remove <p> tag
-
-// make a button to refresh.
-
-
+var episodeDataBase=getAllEpisodes();
+let container=document.querySelector(".episodes-container");
+createPageWithEpisodes(container,episodeDataBase);
 
 
 function setup() {
 
-  let container=document.querySelector(".episodes-container");
   let search=document.querySelector("#search"); // Search Box 
-  let dropDown=document.querySelector("#episodes"); //Drop Down menu
+  let dropDownEpisodes=document.querySelector("#episodes"); //Drop Down menu for episodes
+  let dropDownShowMenu=document.querySelector("#shows"); // Drop down menu for shows
   let refreshButton=document.querySelector(".refresh-button");
 
   refreshButton.addEventListener("click",function(){
 
     location.reload();
   })
+  // Taking all the shows in the variable allShows
+  let allShows=getAllShows();
+  dropDownShows(allShows);
 
-  // const allEpisodes = getAllEpisodes();
-  let allEpisodes;
 
-
-  fetch("https://api.tvmaze.com/shows/82/episodes")
-  .then(function(response)
+  // As soon as a show is selected we go and display episodes of that show.
+  dropDownShowMenu.addEventListener("change",function()
   {
-    return response.json();
+    createPageFromShow(dropDownShowMenu.value , allShows);
+  });
+
+
+
+  // As soon as we enter something in searchBox, this function will be called and page will be updated.
+  search.addEventListener("keyup",function(event){
+    event.preventDefault();
+    let keyword=search.value;
+    if(event.keyCode==13) // this is for pressing enter button.
+    searchAndUpdate(keyword,episodeDataBase);
   })
-  .then(function(data)
+
+  // this event listener is for the select drop down menu of episodes.
+  // it will find the item and display it
+  dropDownEpisodes.addEventListener("change",function()
   {
-    allEpisodes=data;
+    let searchValue= dropDownEpisodes.value.split(" ");
+    searchAndUpdate(searchValue[0],episodeDataBase); // we search only with episode number to be sure while searching so that we get exactly one episode displayed
+  });
 
-    // In this "for loop" we are creating episodes from objects and adding them to the webpage.
-    for(let i=0;i<allEpisodes.length;i++)
-    {
-      let epi=createEpisodeFromObject(allEpisodes[i]);
-      container.appendChild(epi);
-    }
-    // As soon as we enter something in searchBox, this function will be called and page will be updated.
-    search.addEventListener("keyup",function(event){
-      event.preventDefault();
-      let keyword=search.value;
-      if(event.keyCode==13) // this is for pressing enter button.
-      searchAndUpdate(keyword,allEpisodes);
-    })
-
-    // Calling this function will create the dropDown menu with 73 episodes.
-    dropDownMenu(allEpisodes);
-
-    // this event listener is for the select drop down menu
-    // it will find the item and display it
-    dropDown.addEventListener("change",function()
-    {
-      let searchValue= dropDown.value.split(" ");
-      searchAndUpdate(searchValue[0],allEpisodes); // we search only with episode number to be sure while searching so that we get exactly one episode displayed
-    });
-
-  })
-  .catch(function(error)
-  {
-    console.log("Error")
-  })
 }
 
-//This function is used to search.
-function searchAndUpdate(keyword,allEpisodes)
-{
-  let arrayOfKeywords=keyword.split(" ");
-  for(let i=0;i<arrayOfKeywords.length;i++)
-  {
-    searchWord(arrayOfKeywords[i],allEpisodes);
-  }
-}
 
-// This function will search the keyword in the database and update the page
-function searchWord(keyword,allEpisodes)
+
+function createPageFromShow(showSelected,allShows)
 {
+  let showNameAsArray= showSelected.split(" ");
   let container=document.querySelector(".episodes-container");
-  let counter=0;
-  let displayNumber=document.querySelector(".number-of-episodes");
-  // as soon as we enter a value to search we will empty the page.
+
   removeAllChild(container);
 
+    let index   =  showNameAsArray[0];
+    index       =  Number(index); // since it will be a string here it is converted to number.
+    index       =  index-1; // since index is starting from 1 in the drop Down menu we need to subtract 1 in order to use it for the array.
+    
+    let show    =  allShows[index]; // show is the object
+    let SHOW_ID =  show.id;
+
+    let URL="https://api.tvmaze.com/shows/"+SHOW_ID+"/episodes";
+
+    // Here we are fetching the data from the URL of the show. 
+    fetch(URL)
+    .then(function(response){
+      return response.json()
+    })
+    .then(function(data){
+      let allEpisodes=data;
+      episodeDataBase=data; // We are modifying it here so that event listeners in setup function stay updated as well.
+      createPageWithEpisodes(container,allEpisodes);
+    })
+    .catch(function(error){
+      console.log("ERROR")
+    })
+}
+
+
+function createPageWithEpisodes(container,allEpisodes)
+{
   for(let i=0;i<allEpisodes.length;i++)
-  {
-    let ssn=allEpisodes[i].season;
-    let epi=allEpisodes[i].number;
-
-    if(ssn<10)
-      ssn="0"+ssn;
-    if(epi<10)
-      epi="0"+epi;
-
-    // title is made up of name and episode number and season number.
-     let title= allEpisodes[i].name + " "+"S"+ssn+"E"+epi;
-     let description = allEpisodes[i].summary;
-
-    // searchElement is a function that returns true or false depending on, 
-    // if keyWord is found in the given title or description.
-    let found = searchElement(keyword,title,description); 
-
-    if(found) // if found then add it to the page
     {
-      counter++; // Number of episodes containing Keyword.
       let epi=createEpisodeFromObject(allEpisodes[i]);
       container.appendChild(epi);
     }
-  }
-
-  displayNumber.textContent=`Displaying ${counter}/73`;
+    // Calling this function will create the dropDown menu with total episodes.
+    dropDownMenu(allEpisodes);
 }
 
-function removeAllChild(parent)
-{
-  while (parent.firstChild) {
-    parent.removeChild(parent.firstChild);    
-  }
-}
 
-function dropDownMenu(allEpisodes)
-{
-  let dropDownBox=document.querySelector("#episodes");
 
-  for(let i=0;i<allEpisodes.length;i++)
-  {
-    let ssn=allEpisodes[i].season;
-    let epi=allEpisodes[i].number;
-
-    if(ssn<10)
-      ssn="0"+ssn;
-    if(epi<10)
-      epi="0"+epi;
-
-    // title is made up of name and episode number and season number.
-    let title=  "S"+ssn+"E"+epi + " -"+allEpisodes[i].name;
-    let option=document.createElement("OPTION");
-    option.value= title;
-    option.text= title;
-    dropDownBox.appendChild(option);
-    
-  }
-}
-
-// A boolean function which returns either true or false. 
-// returns true if title or description contain the given keyword.
-function searchElement( keyword, title, description)
-{
-  // transforming them to lowercase so that search is not case sensitive.
-  keyword=keyword.toLowerCase();
-  title=title.toLowerCase();
-  description=description.toLowerCase();
-
-  if(title.includes(keyword)  || description.includes(keyword))
-   return true;
-
-  return false;
-}
 
 function createEpisodeFromObject(episodeObj)
 {
@@ -204,34 +141,139 @@ function createEpisodeFromObject(episodeObj)
 
 }
 
+
+
+function dropDownMenu(allEpisodes)
+{
+  let dropDownBox=document.querySelector("#episodes");
+
+  removeAllChild(dropDownBox);
+
+  for(let i=0;i<allEpisodes.length;i++)
+  {
+    let ssn=allEpisodes[i].season;
+    let epi=allEpisodes[i].number;
+
+    if(ssn<10)
+      ssn="0"+ssn;
+    if(epi<10)
+      epi="0"+epi;
+
+    // title is made up of name and episode number and season number.
+    let title=  "S"+ssn+"E"+epi + " -"+allEpisodes[i].name;
+    let option=document.createElement("OPTION");
+    option.value= title;
+    option.text= title;
+    dropDownBox.appendChild(option);
+    
+  }
+}
+
+
+
+
+function removeAllChild(parent)
+{
+  while (parent.firstChild) {
+    parent.removeChild(parent.firstChild);    
+  }
+}
+
+
+
+
+
+
+function dropDownShows(allShows)
+{
+  let dropDownContainer=document.querySelector("#shows");
+
+  for(let i=0;i<allShows.length;i++)
+  {
+    let title=String(i+1) + " ) " + allShows[i].name;
+    let option=document.createElement("OPTION");
+    option.value= title;
+    option.text= title;
+    dropDownContainer.appendChild(option);
+  }
+}
+
+
+
+
+
+
+
+//This function is used to search.
+function searchAndUpdate(keyword,allEpisodes)
+{
+  let arrayOfKeywords=keyword.split(" ");
+  for(let i=0;i<arrayOfKeywords.length;i++)
+  {
+    searchWord(arrayOfKeywords[i],allEpisodes);
+  }
+}
+
+
+
+
+// This function will search the keyword in the database and update the page
+function searchWord(keyword,allEpisodes)
+{
+  let container=document.querySelector(".episodes-container");
+  let counter=0;
+  let displayNumber=document.querySelector(".number-of-episodes");
+  // as soon as we enter a value to search we will empty the page.
+  removeAllChild(container);
+
+  for(let i=0;i<allEpisodes.length;i++)
+  {
+    let ssn=allEpisodes[i].season;
+    let epi=allEpisodes[i].number;
+
+    if(ssn<10)
+      ssn="0"+ssn;
+    if(epi<10)
+      epi="0"+epi;
+
+    // title is made up of name and episode number and season number.
+     let title= allEpisodes[i].name + " "+"S"+ssn+"E"+epi;
+     let description = allEpisodes[i].summary;
+
+    // searchElement is a function that returns true or false depending on, 
+    // if keyWord is found in the given title or description.
+    let found = searchElement(keyword,title,description); 
+
+    if(found) // if found then add it to the page
+    {
+      counter++; // Number of episodes containing Keyword.
+      let epi=createEpisodeFromObject(allEpisodes[i]);
+      container.appendChild(epi);
+    }
+  }
+
+  displayNumber.textContent=`Displaying ${counter}/73`;
+}
+
+
+
+
+
+
+// A boolean function which returns either true or false. 
+// returns true if title or description contain the given keyword.
+function searchElement( keyword, title, description)
+{
+  // transforming them to lowercase so that search is not case sensitive.
+  keyword=keyword.toLowerCase();
+  title=title.toLowerCase();
+  description=description.toLowerCase();
+
+  if(title.includes(keyword)  || description.includes(keyword))
+   return true;
+
+  return false;
+}
+
+
 window.onload = setup;
-
-
-
-
-
-// {
-//   id: 4952,
-//   url:
-//     "http://www.tvmaze.com/episodes/4952/game-of-thrones-1x01-winter-is-coming",
-//   name: "Winter is Coming",
-//   season: 1,
-//   number: 1,
-//   airdate: "2011-04-17", 
-//   airtime: "21:00",        
-//   airstamp: "2011-04-18T01:00:00+00:00", xxxx
-//   runtime: 60,
-//   image: {
-//     medium:
-//       "http://static.tvmaze.com/uploads/images/medium_landscape/1/2668.jpg",
-//     original:
-//       "http://static.tvmaze.com/uploads/images/original_untouched/1/2668.jpg"
-//   },
-//   summary:
-//     "<p>Lord Eddard Stark, ruler of the North, is summoned to court by his old friend, King Robert Baratheon, to serve as the King's Hand. Eddard reluctantly agrees after learning of a possible threat to the King's life. Eddard's bastard son Jon Snow must make a painful decision about his own future, while in the distant east Viserys Targaryen plots to reclaim his father's throne, usurped by Robert, by selling his sister in marriage.</p>",
-//   _links: {
-//     self: {
-//       href: "http://api.tvmaze.com/episodes/4952"
-//     }
-//   }
-// }
